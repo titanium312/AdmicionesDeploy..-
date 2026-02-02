a492f24772fb07687e81bd84af5dc0868a5b0563
Roberto Barreto <rb.roberto.barreto@gmail.com>
2026-02-02 11:06:47 -0500

    Idfacturas actualiza

--- routes/Controller/Base/ids/buscarIdFactura.js
+++ routes/Controller/Base/ids/buscarIdFactura.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const { instituciones } = require("../Instituciones");
+
 async function buscarFactura(req, res) {
-    
     try {
         const { sSearch, idFactura, idInstitucion } = req.body;
 
@@ -19,7 +19,6 @@ async function buscarFactura(req, res) {
             });
         }
 
-        // Buscar el token según el idInstitucion
         const institucion = instituciones.find(inst => inst.idInstitucion === idInstitucion);
         if (!institucion) {
             return res.status(400).json({ 
@@ -29,31 +28,42 @@ async function buscarFactura(req, res) {
 
         const token = institucion.Tksesicion;
 
-        const url = "https://balance.saludplus.co/facturasAdministar/BuscarListadofacturasDatos?fechaInicial=01/01/2020&fechaFinal=01/01/2029&idEntidad=0&idContrato=0&SinNumero=False&duplicadas=False&idCuentaCobro=0&estadoFacturacionElectronica=0";
-        
-        const postData = `sEcho=2&iColumns=8&sColumns=%2CCHECK%2CNUMERO%2CFECHA%2CRESOLUCION%2CPACIENTE%2CENTIDAD%2CESTADO&iDisplayStart=0&iDisplayLength=100&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&sSearch=${sSearch || ''}&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&sSearchable_1=false&sSearch_2=&bRegex_2=false&sSearchable_2=false&sSearch_3=&bRegex_3=false&sSearchable_3=false&sSearch_4=&bRegex_4=false&sSearchable_4=false&sSearch_5=&bRegex_5=false&sSearchable_5=false&sSearch_6=&bRegex_6=false&sSearchable_6=false&sSearch_7=&bRegex_7=false&sSearchable_7=false&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=false&bSortable_2=false&bSortable_3=false&bSortable_4=false&bSortable_5=false&bSortable_6=false&bSortable_7=false`;
+        // Intentar primero con SinNumero = true, luego false
+        const estadosNumero = [true, false];
+        let responseData = null;
 
-        const headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "data": token
-        };
+        for (const estado of estadosNumero) {
+            const url = `https://balance.saludplus.co/facturasAdministar/BuscarListadofacturasDatos?fechaInicial=01/01/2020&fechaFinal=01/01/2029&idEntidad=0&idContrato=0&SinNumero=${estado}&duplicadas=False&idCuentaCobro=0&estadoFacturacionElectronica=0`;
+            
+            const postData = `sEcho=2&iColumns=8&sColumns=%2CCHECK%2CNUMERO%2CFECHA%2CRESOLUCION%2CPACIENTE%2CENTIDAD%2CESTADO&iDisplayStart=0&iDisplayLength=100&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&sSearch=${sSearch || ''}&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&sSearchable_1=false&sSearch_2=&bRegex_2=false&sSearchable_2=false&sSearch_3=&bRegex_3=false&sSearchable_3=false&sSearch_4=&bRegex_4=false&sSearchable_4=false&sSearch_5=&bRegex_5=false&sSearchable_5=false&sSearch_6=&bRegex_6=false&sSearchable_6=false&sSearch_7=&bRegex_7=false&sSearchable_7=false&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=false&bSortable_2=false&bSortable_3=false&bSortable_4=false&bSortable_5=false&bSortable_6=false&bSortable_7=false`;
 
-        const response = await axios.post(url, postData, { headers });
+            const headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "data": token
+            };
 
-        if (!response.data.aaData || response.data.aaData.length === 0) {
+            const response = await axios.post(url, postData, { headers });
+
+            if (response.data.aaData && response.data.aaData.length > 0) {
+                responseData = response.data.aaData;
+                break; // salió bien, no necesitamos seguir buscando
+            }
+        }
+
+        if (!responseData) {
             return res.json({ 
                 error: sSearch ? `No existe factura con número ${sSearch}` : 'No se encontraron facturas'
             });
         }
 
-        const facturas = response.data.aaData.map(f => ({
+        // Mapear facturas
+        const facturas = responseData.map(f => ({
             id: f[0],
             numeroCompleto: f[2],
             numero: f[2] ? f[2].split(' - ')[0] : ''
         }));
 
         let resultado;
-
         if (sSearch) {
             resultado = facturas.find(f => f.numero === sSearch.toString()) || { error: `No se encontró factura con número ${sSearch}` };
         } else if (idFactura) {
@@ -71,5 +81,3 @@ async function buscarFactura(req, res) {
 }
 
 module.exports = { buscarFactura };
-
-
