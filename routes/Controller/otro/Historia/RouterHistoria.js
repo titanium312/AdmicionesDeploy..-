1672fd7d77fb6401ebf3728d60813b1591adf1cb
niniosorio55 <niniosorio55@gmail.com>
2026-08-18 16:27:00 -0500

    corrijamos

--- routes/Controller/otro/Historia/RouterHistoria.js
+++ routes/Controller/otro/Historia/RouterHistoria.js
@@ -1,28 +1,9 @@
 const express = require('express');
 const routerHistoria = express.Router();
 
-// Importar el controlador CORRECTAMENTE
-const { Histronico } = require('../Histronico/Histronico'); // Ajusta la ruta
+const { Hs_Anx } = require('../../historias');
 
-
-
-// También importar el controlador de admisiones
-const { GeneradorHs } = require('./Controller/GeneradorHs'); // Ajusta la ruta
-const { buscarAdmisionMiddleware } = require('./Controller/buscador/buscarIdAdmision');
-
-// Definir rutas
-
-routerHistoria.post('/GeneradorHs', GeneradorHs);
-routerHistoria.post('/buscar', buscarAdmisionMiddleware);
-
-
-
-
-
-
-
-
-routerHistoria.post('/Histronico', Histronico);
+routerHistoria.post('/Histronico', Hs_Anx);
 
 
 module.exports = routerHistoria;
